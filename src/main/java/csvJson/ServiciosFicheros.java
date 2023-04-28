43e88055c49ed27488850030f23ad9d0c6e4da3b
edacalm990 <edacalm990@g.educaand.es>
2023-04-28 11:13:07 +0200

    ejercicio 7F terminado

--- src/main/java/csvJson/ServiciosFicheros.java
+++ src/main/java/csvJson/ServiciosFicheros.java
@@ -4,33 +4,36 @@
  */
 package csvJson;
 
-import java.io.IOException;
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.time.LocalDate;
 
 /**
  *
  * @author eli
  */
 public class ServiciosFicheros {
-        // método para leer archivo llamando al método obtenerLista
-    public static void leerArchivo(String archivo, Charset  codificacion) {
+    // método para leer archivo llamando al método obtenerLista
+
+    public static void leerArchivo(String archivo, Charset codificacion) {
         List<String> lineas = obtenerLista(archivo, codificacion);
         for (String linea : lineas) {
             System.out.println(linea);
         }
     }
-    
-    
-    
-        // método que crea un List<String> con las lineas del archivo
-    public static List<String> obtenerLista(String archivo, Charset  codificacion ) {
+
+    // método que crea un List<String> con las lineas del archivo
+    public static List<String> obtenerLista(String archivo, Charset codificacion) {
         List<String> lineas = new ArrayList<>();
         try {
             lineas = Files.readAllLines(Paths.get(archivo),
@@ -40,9 +43,9 @@ public class ServiciosFicheros {
         }
         return lineas;
     }
-    
+
     // crea un fichero con un String de datos y el nombre
-     public static void crearFichero(String datos, String nombreFichero) {
+    public static void crearFichero(String datos, String nombreFichero) {
 
         try {
             Files.write(Paths.get(nombreFichero), datos.getBytes(StandardCharsets.UTF_8),
@@ -51,8 +54,23 @@ public class ServiciosFicheros {
             System.out.println("Error creando el fichero");
         }
     }
-     
-       
-     
+
+    public static void crearJson(String nombreArchivo, List<Datos> listaDatos) {
+        try {
+            ObjectMapper mapeador = new ObjectMapper();
+
+            // Permite a mapeador usar fechas según java time
+            mapeador.registerModule(new JavaTimeModule());
+
+            // Formato JSON bien formateado. Si se comenta, el fichero queda minificado
+            mapeador.configure(SerializationFeature.INDENT_OUTPUT, true);
+
+            // Escribe en un fichero JSON el catálogo de muebles
+            mapeador.writeValue(new File(nombreArchivo), listaDatos);
+        } catch (Exception e) {
+            System.out.println("No se ha podido crear el archivo JSON");
+        }
+
+    }
 
 }
--- src/main/java/csvJson/mainCsvJson.java
+++ src/main/java/csvJson/mainCsvJson.java
@@ -5,8 +5,12 @@
 package csvJson;
 
 import java.nio.charset.StandardCharsets;
+import java.time.LocalDate;
+import java.time.Period;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  *
@@ -23,16 +27,132 @@ contando a partir de hoy, lleven trabajando más de 10 y menos de 15 años (ten 
 Las lecturas y escrituras de los ficheros se realizan de forma relativa a la carpeta raíz del proyecto Java. 
 Cuanto más modularizado esté todo mejor.
 
-*/
+ */
+ /*
+a) Sin usar API Stream
+
+ - Contar el número de profesores de Tecnología.
+ - Saber si algún profesor/a de Informática es también coordinador
+ - Obtener una lista ordenada alfabéticamente con todos los apellidos de los empleados cuyo NIF contenga la letra J.
+ - Verificar que ningún profesor se llama "Jonh".
+
+b) Repetir el apartado a) usando API Stream
+ */
 public class mainCsvJson {
+
     public static void main(String[] args) {
-        List<String> listaString= ServiciosFicheros.obtenerLista("./datosPersonal.csv", StandardCharsets.ISO_8859_1);
-        List<Datos> listaDatos=new ArrayList<>();
-        for (int i = 0; i < listaString.size(); i++) {
-            String [] datosSeparados= listaString.get(i).split("\".\"");
-            Datos tmp=new Datos();
-            
+        List<String> listaString = ServiciosFicheros.obtenerLista("./datosPersonal.csv", StandardCharsets.ISO_8859_1);
+        List<Datos> listaDatos = crearListaDatos(listaString);
+        List<Datos> listaDatosFiltrada = eliminarTrabajadoresAntiguedad(listaDatos, 10, 15);
+        System.out.println(listaDatosFiltrada.size());
+        ServiciosFicheros.crearJson("trabajadores.json", listaDatosFiltrada);
+
+        // A  - 
+        //Contar el número de profesores de Tecnología.
+        int cantidadProfesores= contarProfesores("Tecnología", listaDatos);
+        //Saber si algún profesor/a de Informática es también coordinador
+        int cantidadProfesoresInformaticosCoordinadores= contarProfesoresEsCordinador("Informática", listaDatos);
+        //  - Obtener una lista ordenada alfabéticamente con todos los apellidos de los empleados cuyo NIF contenga la letra J.
+        listaDatos.sort((x,y)->x.getNombreEmpleado().compareToIgnoreCase(y.getNombreEmpleado()));
+        List <String> listaDatosOrdenadaYFiltrada=new ArrayList<>();
+        for (Datos empleado : listaDatos) {
+            if(empleado.getDNI_Pasaporte().contains("J")){
+            listaDatosOrdenadaYFiltrada.add(empleado.getNombreEmpleado().split(" ")[0]);
+            }
         }
         
+        listaDatosOrdenadaYFiltrada.forEach(System.out::println);
+        // Verificar que ningún profesor se llama "Jonh".
+        System.out.println(existe("jonh", listaDatos));
+        
+        // B 
+        //Contar el número de profesores de Tecnología.
+        System.out.println(listaDatos.stream().filter(x->x.getPuesto().contains("Tecnología")).count());
+        //Saber si algún profesor/a de Informática es también coordinador
+        System.out.println(listaDatos.stream().filter(x->x.getPuesto().contains("Informática")).anyMatch((t) -> t.isCoordinador()));
+        // Obtener una lista ordenada alfabéticamente con todos los apellidos de los empleados cuyo NIF contenga la letra J.
+        List<String> apellidos=listaDatos.stream()
+                .filter(x->x.getDNI_Pasaporte().contains("J"))
+                .map(x-> x.getNombreEmpleado().split(" ")[0])
+                .sorted()
+                .collect(Collectors.toList());
+        apellidos.forEach(System.out::println);
+        // // Verificar que ningún profesor se llama "Jonh".
+        System.out.println(listaDatos.stream().anyMatch(x->x.getNombreEmpleado().split(" ")[2].contains("Jonh")));
+        
+        
     }
+
+    public static List<Datos> crearListaDatos(List<String> listaString) {
+        List<Datos> listaDatos = new ArrayList<>();
+        for (int i = 1; i < listaString.size(); i++) {
+            String[] datosSeparados = listaString.get(i).split("\".\"");
+            Datos tmp = new Datos();
+
+            tmp.setNombreEmpleado(datosSeparados[0].replace("\"", ""));
+            tmp.setDNI_Pasaporte(datosSeparados[1]);
+            tmp.setPuesto(datosSeparados[2]);
+            if (!datosSeparados[3].isBlank()) {
+                tmp.setFechaInicio(LocalDate.parse(datosSeparados[3], DateTimeFormatter.ofPattern("dd/MM/yyyy")));
+            }
+            if (!datosSeparados[4].isBlank()) {
+                tmp.setFechaFin(LocalDate.parse(datosSeparados[4], DateTimeFormatter.ofPattern("dd/MM/yyyy")));
+            }
+            tmp.setTelefono(datosSeparados[5]);
+            if (!datosSeparados[5].isBlank()) {
+                tmp.setCoordinador(datosSeparados[6].equalsIgnoreCase("No")
+                        ?false
+                        :true);
+            }
+            listaDatos.add(tmp);
+        }
+        return listaDatos;
+    }
+
+    public static List<Datos> eliminarTrabajadoresAntiguedad(List<Datos> listaDatos, int inicio, int fin) {
+        return listaDatos.stream()
+                .filter((t)
+                        -> Period.between(t.getFechaInicio(),
+                        ((t.getFechaFin() != null)
+                        ? t.getFechaFin()
+                        : LocalDate.now())).getYears() >= inicio
+                && Period.between(t.getFechaInicio(),
+                        ((t.getFechaFin() != null)
+                        ? t.getFechaFin()
+                        : LocalDate.now())).getYears() <= fin
+                )
+                .toList();
+    }
+
+    public static int contarProfesores(String tipo, List<Datos> listaDatos) {
+        int contador = 0;
+        for (Datos dato : listaDatos) {
+            if(dato.getPuesto().contains(tipo)){
+            contador ++;
+            };
+        }
+        
+        return contador;
+    }
+    
+        public static int contarProfesoresEsCordinador(String tipo, List<Datos> listaDatos) {
+        int contador = 0;
+        for (Datos dato : listaDatos) {
+            if(dato.getPuesto().contains(tipo) && dato.isEvaluador()){
+            contador ++;
+            };
+        }
+        
+        return contador;
+    }
+        
+        public static boolean existe(String nombre, List<Datos> listaDatos){
+            for (Datos empleado : listaDatos) {
+                if(empleado.getNombreEmpleado().toLowerCase().contains(nombre.toLowerCase())){
+                return true;
+                }
+            }
+            return false;
+        }
+    
 }
